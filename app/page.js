bdd4bc7884d3401e074246e51125b7e0e0fd544b
mohammad syed hasan <syedhasanmohammad@gmail.com>
2024-11-30 23:40:41 +0600

    designed header in dasgboard

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
-import Image from "next/image";
+"use client";
+import React from "react";
 import Link from "next/link";
 
-export default function Home() {
+const Home = () => {
   return (
     <div className="flex items-center flex-col justify-center min-h-screen ">
       <h2 className="text-3xl mb-4 ">InvenTrack</h2>
@@ -10,4 +11,5 @@ export default function Home() {
       </Link>
     </div>
   );
-}
+};
+export default Home;
--- components/dashboard/header/Header.jsx
+++ components/dashboard/header/Header.jsx
@@ -1,20 +1,32 @@
 "use client";
 
 import SearchInput from "@/components/shared/SearchInput/SearchInput";
-import { Bell, History, Plus, Settings, User2, Users } from "lucide-react";
+import {
+  Bell,
+  ChevronDown,
+  History,
+  LayoutGrid,
+  Plus,
+  Settings,
+  Users,
+} from "lucide-react";
+import Image from "next/image";
 import React from "react";
+import { Tooltip } from "react-tooltip";
 
 const Header = () => {
   return (
-    <div className="bg-gray-50 h-14 flex items-center justify-between px-5">
+    <div className="bg-gray-50 text-slate-900 h-14 flex items-center justify-between px-5 border-b border-slate-200  shadow-sm">
       {/* segment */}
-      <div className="flex gap-3 items-center">
-        {/* recent activity */}
-        {/* recent history icon from lucide */}
-        <History className="w-7 h-7" />
+      <div className="grow flex-1 flex ">
+        <div className="flex gap-3 items-center ">
+          {/* recent activity */}
+          {/* recent history icon from lucide */}
+          <History className="w-7 h-7" />
 
-        {/* Search Box */}
-        <SearchInput></SearchInput>
+          {/* Search Box */}
+          <SearchInput></SearchInput>
+        </div>
       </div>
       {/* 2nd segment */}
       <div className="flex px-2  items-center gap-2">
@@ -23,12 +35,14 @@ const Header = () => {
           {/* <!-- Show tooltip on bottom --> */}
 
           <button
-            data-tooltip-target="tooltip-bottom"
-            data-tooltip-placement="bottom"
+            data-tooltip-id="PlusBtnTooltip"
+            data-tooltip-content="Quick access"
+            data-tooltip-place="bottom"
             type="button"
             className="px-1 py-1 group text-white bg-blue-500 rounded-lg hover:bg-blue-600 transition duration-200"
           >
             <Plus className="group-hover:scale-105 text-slate-50 w-4 h-4" />
+            <Tooltip id="PlusBtnTooltip" />
           </button>
           {/* tooltip */}
           <div
@@ -43,8 +57,7 @@ const Header = () => {
         {/* users btn  /* bell * */
         /* bell */}
         <div className="flex gap-2 px-2 border-r border-r-gray-300">
-          {/* <!-- Show tooltip on bottom --> */}
-
+          {/* users btn */}
           <button
             type="button"
             className="px-1 py-1 group rounded-lg hover:bg-slate-200 transition duration-200"
@@ -55,23 +68,45 @@ const Header = () => {
           {/* bell */}
           <button
             type="button"
-            className="px-1 py-1 group rounded-lg hover:bg-slate-200 transition duration-200"
+            className="px-1 group py-1 group rounded-lg hover:bg-slate-200 transition duration-200"
           >
-            <Bell className="group-hover:scale-105 text-slate-900 w-4 h-4" />
+            <Bell className="group-hover:scale-105 group-hover:-rotate-12  transition-transform duration-700 text-slate-900 w-4 h-4" />
           </button>
           {/* settings */}
           <button
             type="button"
-            className="px-1 py-1 group rounded-lg hover:bg-slate-200 transition duration-200"
+            className="px-1 group py-1 group rounded-lg hover:bg-slate-200 transition duration-200"
           >
-            <Settings className="group-hover:scale-105 text-slate-900 w-4 h-4" />
+            <Settings className="group-hover:scale-105 group-hover:rotate-180 transition-transform duration-700 text-slate-900 w-4 h-4" />
           </button>
         </div>
 
         {/*  */}
       </div>
       {/* 3rd sections */}
-      <div></div>
+      <div className="flex gap-6">
+        {/* Garat name of organization */}
+        <button className="flex-row-reverse text-slate-900 flex items-center gap-1">
+          <span>Garat</span>
+          <ChevronDown className="text-xs" />
+        </button>
+
+        {/* image */}
+        <button>
+          <Image
+            width={96}
+            height={96}
+            className="rounded-full w-8 h-8 border border-slate-800 "
+            alt="user image"
+            src="/user.png"
+          />
+        </button>
+
+        {/* grid button */}
+        <button>
+          <LayoutGrid className="w-6 h-6 text-slate-900" />
+        </button>
+      </div>
     </div>
   );
 };
